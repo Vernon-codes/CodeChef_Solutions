2a7ce0217c71d5b3df2f0e1ddd60fb7ce9503af4
Vernon Alva <codewithvernon@gmail.com>
2026-04-14 22:51:38 +0530

    added_Content_module_4

--- Problem_Solving_in_Python/Module4_Logic_Building.py
+++ Problem_Solving_in_Python/Module4_Logic_Building.py
@@ -1,10 +1,25 @@
 #MOVIE-2X
 x=int(input())
 y=int(input())
-
 if y%2==0:
     x=x-y
     z=y//2 #2x speed means half the time
 else:
     print("Invalid Input")
 print("Total Minutes Spent: ",x+z)
+
+#ROWS AND CELLS
+t=int(input())
+for i in range(t):
+    a,b,c,d=map(int,input().split())
+    if a==c and b==d:
+        print(1)
+    else:
+        print(2)
+
+#TOO MANY FLOORS
+t = int(input())
+for _ in range(t):
+    x, y = map(int, input().split())
+    print(abs((x - 1)//10 - (y - 1)//10)) #find the floor no of each and then subract it with abs 
+
